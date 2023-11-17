f5134ab6cb2f20bba899463716547e77c890bf9d
minzai0116 <63540495+minzai0116@users.noreply.github.com>
2023-11-17 11:04:11 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-pip install sphinx-rtd-theme
 import sphinx_rtd_theme
 html_theme = 'python_docs_theme'
 html_static_path = ['_static']
